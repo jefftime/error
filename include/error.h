0827ee0b51c5a706edadf327d3e293f725276a10
Jeffery Stager <jffry09@gmail.com>
2019-06-05 23:05:46 -0400

    added parens around expr for safety

--- include/error.h
+++ include/error.h
@@ -1,6 +1,7 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-#define chkerr(expr, fail) { int err = expr; if (err) { fail; return err; } }
+#define chkerrf(expr, fail) { int err = (expr); if (err) { fail; return err; } }
+#define chkerr(expr) { int err; if ((err = (expr))) return err; }
 
 #endif
