2beb7ab9ccc569df3cf15185c084de54ca58a3f1
Jeffery Stager <jffry09@gmail.com>
2019-08-03 17:27:21 -0400

    increased readability

--- include/error.h
+++ include/error.h
@@ -2,6 +2,6 @@
 #define ERROR_H
 
 #define chkerrf(expr, fail) { int err = (expr); if (err) { fail; return err; } }
-#define chkerr(expr) { int err; if ((err = (expr))) return err; }
+#define chkerr(expr) { int err = (expr); if (err) return err; }
 
 #endif
